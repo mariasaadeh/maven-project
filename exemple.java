948f22f5b857144ca6b2a59caa95a479e82b2946
mariasaadeh <114923492+mariasaadeh@users.noreply.github.com>
2022-10-05 15:41:58 +0200

    Create exemple.java

---
+++ exemple.java
@@ -0,0 +1,15 @@
+package com.example ;
+import javax.swing.JOptionPane ;
+
+ 
+
+public class Exemple {
+ 
+  public static void main (String[] args) {
+    // todo
+    system.out.println("Hello World") ;
+    try {
+    }catch(Exception e) {
+    }
+  }
+}
